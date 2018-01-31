b6f433a15325f5c05b92cb5c78666353d0fe1d6b
Jason Prasad <jason.prasad@gm.com>
2018-01-31 10:28:58 -0500

    adding autojump

--- oh-my-zshrc.nix
+++ oh-my-zshrc.nix
@@ -1,10 +1,9 @@
 with import <nixpkgs> {};
 
-pkgs.writeText "zshrc" ''
+pkgs.writeText "oh-my-zshrc" ''
 ZSH_THEME="theunraveler"
 export ZSH=${pkgs.oh-my-zsh}/share/oh-my-zsh/
-export NIX_PATH="nixpkgs=$HOME/.nix-defexpr/channels"
 source $ZSH/oh-my-zsh.sh
-source ~/.nix-profile/etc/profile.d/*.sh
+source ${pkgs.autojump}/share/autojump/autojump.zsh
 plugins=(git tmux)
 ''
