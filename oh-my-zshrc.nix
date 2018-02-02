126585d1396c05000a1b5f935a80c02d443fcb84
Jason Prasad <jason.prasad@gm.com>
2018-02-02 12:44:27 -0500

    oh-my-zshrc

--- oh-my-zshrc.nix
+++ oh-my-zshrc.nix
@@ -6,4 +6,9 @@ export ZSH=${pkgs.oh-my-zsh}/share/oh-my-zsh/
 source $ZSH/oh-my-zsh.sh
 source ${pkgs.autojump}/share/autojump/autojump.zsh
 plugins=(git tmux)
+alias c="clear"
+alias up="cd .."
+alias fuxcode="rm -rf ~/Library/Developer/Xcode/DerivedData"
+export CONDA="$HOME/miniconda2/bin"
+export PATH="$CONDA:$PATH"
 ''
